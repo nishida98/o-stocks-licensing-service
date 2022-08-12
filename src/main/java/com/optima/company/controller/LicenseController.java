df1232ab79d9e4f8d5663e7b02921345523df5b1
Leonardo Nishida <leonardo.nishida@provider-it.com.br>
2022-08-12 17:30:10 -0300

    Hateos implemented

--- src/main/java/com/optima/company/controller/LicenseController.java
+++ src/main/java/com/optima/company/controller/LicenseController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Locale;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.reactive.WebFluxLinkBuilder.methodOn;
+
 @RestController
 @RequestMapping(value = "v1/organization/{organizationId}/license")
 public class LicenseController {
@@ -21,6 +24,20 @@ public class LicenseController {
             @PathVariable("licenseId") String licenseId) {
 
         License license = licenseService.getLicense(licenseId,organizationId);
+
+        license.add(linkTo(methodOn(LicenseController.class)
+                        .getLicense(organizationId, license.getLicenseId()))
+                        .withSelfRel(),
+                linkTo(methodOn(LicenseController.class)
+                        .createLicense(organizationId, license, null))
+                        .withRel("createLicense"),
+                linkTo(methodOn(LicenseController.class)
+                        .updateLicense(organizationId, license, null))
+                        .withRel("updateLicense"),
+                linkTo(methodOn(LicenseController.class)
+                        .deleteLicense(organizationId, license.getLicenseId(), null))
+                        .withRel("deleteLicense"));
+
         return ResponseEntity.ok(license);
 
     }
