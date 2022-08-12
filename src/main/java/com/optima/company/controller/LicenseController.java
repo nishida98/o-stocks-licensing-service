4953dda75c3f39575e3e3e5b85a7df0d4156e159
Leonardo Nishida <leonardo.nishida@provider-it.com.br>
2022-08-12 16:47:01 -0300

    Dummy data for the endpoints and locale management

---
+++ src/main/java/com/optima/company/controller/LicenseController.java
@@ -0,0 +1,58 @@
+package com.optima.company.controller;
+
+import com.optima.company.model.License;
+import com.optima.company.service.LicenseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
+
+@RestController
+@RequestMapping(value = "v1/organization/{organizationId}/license")
+public class LicenseController {
+
+    @Autowired
+    private LicenseService licenseService;
+
+    @GetMapping(value="/{licenseId}")
+    public ResponseEntity<License> getLicense(
+            @PathVariable ("organizationId") String organizationId,
+            @PathVariable("licenseId") String licenseId) {
+
+        License license = licenseService.getLicense(licenseId,organizationId);
+        return ResponseEntity.ok(license);
+
+    }
+
+    @PutMapping
+    public ResponseEntity<String> updateLicense(
+            @PathVariable("organizationId") String organizationId,
+            @RequestBody License request,
+            @RequestHeader(value = "Accept-Language", required = false) Locale locale) {
+
+        return ResponseEntity.ok(licenseService.updateLicense(request, organizationId, locale));
+
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createLicense(
+            @PathVariable("organizationId") String organizationId,
+            @RequestBody License request,
+            @RequestHeader(value = "Accept-Language", required = false) Locale locale) {
+
+        return ResponseEntity.ok(licenseService.createLicense(request, organizationId, locale));
+
+    }
+
+    @DeleteMapping(value="/{licenseId}")
+    public ResponseEntity<String> deleteLicense(
+            @PathVariable("organizationId") String organizationId,
+            @PathVariable("licenseId") String licenseId,
+            @RequestHeader(value = "Accept-Language", required = false) Locale locale) {
+
+        return ResponseEntity.ok(licenseService.deleteLicense(licenseId, organizationId, locale));
+
+    }
+
+}
